ade1aa04b62f5c8cecb97f3e114b49e9f2bc73e6
Majok-Bol <bolmajok001@gmail.com>
2025-04-02 19:09:02 +0300

    added images to image folder

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
-  final List<String>images=[];
+  final List<String>images=[
+    "assets/images/toy7.jpeg"
+        "assets/images/toy9.jpeg",
+        "assets/images/toy1.webp",
+        "assets/images/toy4.webp",
+        "assets/images/toy5.webp"
+        "assets/images/toy6.webp",
+        "assets/images/toy8.webp",
+        "assets/images/toy10.webp",
+        "images/a_dot_ham.jpeg",
+  ];
   @override
   Widget build(BuildContext context){
     return MaterialApp(
