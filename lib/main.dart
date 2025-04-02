96650937165e035117ad572d8318dd7e380b10a4
Majok-Bol <bolmajok001@gmail.com>
2025-04-02 19:39:13 +0300

    added images with index kept on each iteration

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,17 @@ void main(){
 }
 class MyApp extends StatelessWidget{
   final List<String>images=[
-    "assets/images/toy7.jpeg"
-        "assets/images/toy9.jpeg",
         "assets/images/toy1.webp",
+    "assets/images/toy2.jpeg",
+    "assets/images/toy3.webp",
         "assets/images/toy4.webp",
-        "assets/images/toy5.webp"
+        "assets/images/toy5.jpeg",
         "assets/images/toy6.webp",
+    "assets/images/toy7.jpg",
         "assets/images/toy8.webp",
+    "assets/images/toy9.jpeg",
         "assets/images/toy10.webp",
-        "images/a_dot_ham.jpeg",
+
   ];
   @override
   Widget build(BuildContext context){
@@ -20,6 +22,23 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title:  Text('Toy shop app',style: TextStyle(color: Colors.white,fontSize: 25),),centerTitle: true,backgroundColor: Colors.purple,),
+        body: ListView.builder(
+          itemCount: images.length,
+            prototypeItem: ListTile(
+              leading: Icon(Icons.toys),
+              title:Text('Sample toy'),
+
+            ),
+            itemBuilder:(context,index){
+            return ListTile(
+              contentPadding: EdgeInsets.all(10),
+              leading: Image.asset(images[index]),
+              title: Text('Toy ${index+1}'),
+
+            );
+
+
+        }),
         
       ),
     );
